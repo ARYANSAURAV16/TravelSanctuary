12ae24573efe67affc2cecf19ffdff7648b28518
Aryan Saurav <75740236+ARYANSAURAV16@users.noreply.github.com>
2023-08-19 16:26:53 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:3001/api/v1'
+//export const BASE_URL = 'http://localhost:3001/api/v1'
+export const BASE_URL='https://travel-sanctuary.vercel.app/'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
